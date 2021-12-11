189d52fe6a93a5d8036da9e0209187f576d725f7
Matt <matthiasvstephens@gmail.com>
2021-12-11 03:20:26 +0100

    Fix CSP issue

--- server/index.js
+++ server/index.js
@@ -51,7 +51,13 @@ app.enable('trust proxy');
 app.use(rateLimiter);
 app.use(speedLimiter);
 
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: false,
+//   })
+// );
 app.use(helmet());
+
 app.use(logger('dev'));
 app.use(compression());
 
